d0cb7519e805798ac407ca7a922fd7d0b006b50f
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-16 13:40:14 -0400

    Create bears table and navigate it

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 3, 'M', 'blue', 'mean', true),
+  ('Rowdy', 3, 'M', 'blue', 'mean', true),
+  ('Tabitha', 3, 'M', 'blue', 'mean', true),
+  ('Sergeant Brown', 3, 'M', 'blue', 'mean', true),
+  ('Melissa', 3, 'M', 'blue', 'mean', true),
+  ('Grinch', 3, 'M', 'blue', 'mean', true),
+  ('Wendy', 3, 'M', 'blue', 'mean', true),
+  (null, 3, 'M', 'blue', 'mean', true);
